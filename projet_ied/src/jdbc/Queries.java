16f9eb1a21eb65b87d690584ba97dea7ffde4f95
elise <elise@DESKTOP-CFUAO28>
2020-04-29 18:56:05 +0200

    Ajout d'une query qui ne trouve les infos que pour un seul film

--- projet_ied/src/jdbc/Queries.java
+++ projet_ied/src/jdbc/Queries.java
@@ -46,6 +46,41 @@ public class Queries {
 		return films;
 	}
 	
+	public static Film getFilm(Film film) {
+		
+			String title = film.getTitre();
+			String regex = "^";
+			for(int j=0 ; j<title.length() ; j++) {
+				regex += "("+title.toLowerCase().charAt(j)+"|"+title.toUpperCase().charAt(j)+")";
+			}
+			
+			try {
+				String getFilmsQuery = "SELECT titre, date_sortie, genre, distributeur, budget, revenus_etats_unis, revenus_mondiaux FROM film WHERE titre REGEXP ?;";
+	
+				Connection dbConnection = JdbcConnection.getConnection();
+				PreparedStatement preparedStatement = dbConnection.prepareStatement(getFilmsQuery);
+	
+				preparedStatement.setString(1, regex);
+	
+				ResultSet result = preparedStatement.executeQuery();
+	
+				while (result.next()) {
+					film.setDateSortie(result.getDate("date_sortie"));
+					film.setGenre(result.getString("genre"));
+					film.setDistributeur(result.getString("distributeur"));
+					film.setBudget(result.getDouble("budget"));
+					film.setRevenusEtatsUnis(result.getDouble("revenus_etats_unis"));
+					film.setRevenusMondiaux(result.getDouble("revenus_mondiaux"));
+				}
+	
+				preparedStatement.close();
+			} catch (SQLException se) {
+				System.err.println(se.getMessage());
+			}
+		
+		return film;
+	}
+	
 	public static ArrayList<String> getCsvInformation() {
 		ArrayList<String> csvInformation = new ArrayList<String>();
 		try {
